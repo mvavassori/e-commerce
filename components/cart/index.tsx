f88a7690168b8a263675dafae3ab8a6d926e42d3
Marco Vavassori <mv@marcovavassori.com>
2023-12-04 11:27:34 +0100

    close modals on click outside

--- components/cart/index.tsx
+++ components/cart/index.tsx
@@ -8,37 +8,36 @@ import CartIcon from "../icons/CartIcon";
 export default function Cart() {
   const [isOpen, setIsOpen] = useState(false);
   const toggleCartMenu = () => setIsOpen(!isOpen);
+
+  // Close the cart when the overlay is clicked
+  const closeCart = () => setIsOpen(false);
+
   return (
     <div className="">
-      <div className="flex w-full">
+      {/* Overlay */}
+      {isOpen && (
         <div
-          className={`fixed top-0 left-0 bottom-0 w-5/6 max-w-sm bg-white z-20 transform ${
-            isOpen ? "translate-x-0" : "-translate-x-full"
+          className="fixed inset-0 bg-black bg-opacity-50 z-10"
+          onClick={closeCart}
+        ></div>
+      )}
+
+      <div className="flex w-full">
+        {/* Cart Modal */}
+        <div
+          className={`fixed top-0 right-0 bottom-0 w-5/6 max-w-sm bg-white z-20 transform ${
+            isOpen ? "translate-x-0" : "translate-x-full"
           } transition-transform duration-300 ease-in-out`}
         >
-          {/* Menu Items */}
-          <div className="mt-12 flex flex-col items-start p-4">
-            <Link
-              href="/"
-              className="text-lg block p-2 w-full"
-              onClick={toggleCartMenu}
-            >
-              Home
-            </Link>
-            <Link
-              href="/about"
-              className="text-lg block p-2 w-full"
-              onClick={toggleCartMenu}
-            >
-              About
-            </Link>
-            {/* Add other links similarly */}
-          </div>
+          <button onClick={toggleCartMenu} className="absolute top-5 left-5">
+            <CloseIcon />
+          </button>
+          {/* TODO cart elements */}
         </div>
-        {/* Toggle Button */}
       </div>
-      <button onClick={toggleCartMenu} className="z-30">
-        {isOpen ? <CloseIcon /> : <CartIcon />}
+      {/* Toggle Button */}
+      <button onClick={toggleCartMenu} className="">
+        <CartIcon />
       </button>
     </div>
   );
