999b9fca44ac1ecc79329a131f5259b26dbf1a0c
Marco Vavassori <mv@marcovavassori.com>
2023-12-19 16:33:51 +0100

    empty orders

--- components/DashboardOrders.tsx
+++ components/DashboardOrders.tsx
@@ -36,47 +36,58 @@ const DashboardOrders = async () => {
       <h2 className="font-bold text-3xl">Orders</h2>
       <div className="mt-4">
         {/* ...other user dashboard content... */}
-        {orders.map((order) => (
-          <div key={order.id} className="mb-4">
-            <h3 className="font-bold">
-              Order #{order.id} - Status: {order.status}
-            </h3>
-            {order.items.map((item) => (
-              <div
-                key={item.id}
-                className="flex items-center border rounded-md p-2"
-              >
-                <Image
-                  src={
-                    item.productVariant?.product.images[0].url ||
-                    "https://iskaknozihhuhqaiqtlj.supabase.co/storage/v1/object/public/e-commerce%20images/no-image.png?t=2023-12-19T13%3A41%3A15.217Z"
-                  }
-                  alt={
-                    item.productVariant?.product.name || "Default Product Name"
-                  }
-                  width={100}
-                  height={100}
-                  className="w-20 h-20 object-cover mr-4 rounded-md"
-                />
-                <div>
-                  <p>Product: {item.productVariant?.product.name}</p>
-                  <p>Quantity: {item.quantity}</p>
-                  <p>Price: ${item.price}</p>
-                  <p className="">
-                    Attributes:{" "}
-                    {item.productVariant?.attributes &&
-                      typeof item.productVariant.attributes === "object" &&
-                      Object.entries(item.productVariant.attributes)
-                        .map(([attributeKey, attributeValue]) =>
-                          attributeValue?.toString()
-                        )
-                        .join(" / ")}
-                  </p>
+
+        {orders.length > 0 ? (
+          orders.map((order) => (
+            <div key={order.id} className="mb-4">
+              <h3 className="font-bold">
+                Order #{order.id} - Status: {order.status}
+              </h3>
+              <p className="text-xs text-gray-700">
+                Date: {order.createdAt.toLocaleDateString()}
+              </p>
+              {order.items.map((item) => (
+                <div
+                  key={item.id}
+                  className="flex items-center border rounded-md p-2"
+                >
+                  <Image
+                    src={
+                      item.productVariant?.product.images[0].url ||
+                      "https://iskaknozihhuhqaiqtlj.supabase.co/storage/v1/object/public/e-commerce%20images/no-image.png?t=2023-12-19T13%3A41%3A15.217Z"
+                    }
+                    alt={
+                      item.productVariant?.product.name ||
+                      "Default Product Name"
+                    }
+                    width={100}
+                    height={100}
+                    className="w-20 h-20 object-cover mr-4 rounded-md"
+                  />
+                  <div>
+                    <p>Product: {item.productVariant?.product.name}</p>
+                    <p>Quantity: {item.quantity}</p>
+                    <p>Price: ${item.price}</p>
+                    <p className="">
+                      Attributes:{" "}
+                      {item.productVariant?.attributes &&
+                        typeof item.productVariant.attributes === "object" &&
+                        Object.entries(item.productVariant.attributes)
+                          .map(([attributeKey, attributeValue]) =>
+                            attributeValue?.toString()
+                          )
+                          .join(" / ")}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-        ))}
+              ))}
+            </div>
+          ))
+        ) : (
+          <p className="text-lg font-semibold text-gray-700">
+            We haven&apos;t received any orders yet...
+          </p>
+        )}
       </div>
     </div>
   );
