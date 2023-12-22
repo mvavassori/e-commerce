f64ecbca159f21b63768b8f6ea5dbb787a8aca63
Marco Vavassori <mv@marcovavassori.com>
2023-12-22 14:55:37 +0100

    fake legal pages

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,53 @@
+export default function PrivacyPolicy() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Privacy Policy</h1>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Introduction</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">
+          Information Collection and Use
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+
+      {/* Repeat for other sections */}
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Contact Information</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+    </div>
+  );
+}
---
+++ app/terms-of-service/page.tsx
@@ -0,0 +1,65 @@
+export default function TermsOfService() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold">Terms and Conditions</h1>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Introduction</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Communications</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+
+      {/* Repeat for other sections */}
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Purchases</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+
+      <section className="mt-4">
+        <h2 className="text-xl font-semibold">Refunds</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero
+          justo laoreet sit amet cursus sit. Dictumst quisque sagittis purus sit
+          amet volutpat consequat. Egestas diam in arcu cursus euismod. Sed
+          faucibus turpis in eu mi bibendum. Consectetur libero id faucibus
+          nisl. Quisque id diam vel quam elementum. Eros donec ac odio tempor
+          orci dapibus ultrices. Turpis tincidunt id aliquet risus. Pellentesque
+          eu tincidunt tortor aliquam nulla facilisi cras fermentum odio.
+        </p>
+      </section>
+    </div>
+  );
+}
